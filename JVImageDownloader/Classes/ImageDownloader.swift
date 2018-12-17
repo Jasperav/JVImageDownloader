61198141dd3cc3f266758bc373cc7ee474cc77b4
Jasper <Jasperav@hotmail.com>
2018-12-17 20:11:37 +0100

    commit

--- JVImageDownloader/Classes/ImageDownloader.swift
+++ JVImageDownloader/Classes/ImageDownloader.swift
@@ -3,9 +3,8 @@ import NVActivityIndicatorView
 import JVConstraintEdges
 
 open class ImageDownloader: UIView {
-    
-    private let contentType = NVActivityIndicatorType.ballScaleMultiple
-    private let loader = NVActivityIndicatorView(frame: frame, type: contentType, color: .black, padding: 0)
+
+    private let loader = NVActivityIndicatorView(frame: frame, type: .ballScaleMultiple, color: .black, padding: 0)
     private let imageView = UIImageView()
     
     public init(NVActivityIndicatorType: NVActivityIndicatorType){
